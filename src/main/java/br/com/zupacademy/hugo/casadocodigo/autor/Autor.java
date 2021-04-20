ecebf01d296b36eaeb68644122084ca0d08655ad
hugovallada <valladahugo@gmail.com>
2021-04-20 08:46:23 -0300

    Cria o Autor, DTO de entrada com validações, Controller e Repository do Autor

---
+++ src/main/java/br/com/zupacademy/hugo/casadocodigo/autor/Autor.java
@@ -0,0 +1,30 @@
+package br.com.zupacademy.hugo.casadocodigo.autor;
+
+import javax.persistence.*;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, length = 400)
+    private String descricao;
+
+    public Autor(String nome, String email, String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
+
+    @Deprecated
+    public Autor() {
+    }
+}
---
+++ src/main/java/br/com/zupacademy/hugo/casadocodigo/autor/AutorController.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.hugo.casadocodigo.autor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/autor")
+public class AutorController {
+
+    @Autowired
+    private AutorRepository autorRepository;
+
+    @PostMapping
+    public void criarAutor(@RequestBody @Valid AutorRequestDTO autorDto){
+        autorRepository.save(autorDto.toModel());
+    }
+}
